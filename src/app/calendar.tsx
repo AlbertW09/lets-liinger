3df49b0e4e6d2db1ca52ad90b9963f3101305908
AlbertW09 <awcoder153@gmail.com>
2026-07-21 21:05:57 -0700

    placeholder pages

---
+++ src/app/calendar.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export default function CalendarPage() {
+  return <Text>Calendar page</Text>;
+}
---
+++ src/app/map.tsx
@@ -0,0 +1,5 @@
+import { Text } from 'react-native';
+
+export default function MapPage() {
+  return <Text>Map page</Text>;
+}
